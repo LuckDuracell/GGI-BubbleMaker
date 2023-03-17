ac73a462bfd9e4b843ae2aa353c90990e7dcc42e
LuckDev <lukedru@yahoo.com>
2023-03-16 17:32:44 -0700

    Initial Commit

---
+++ GGI BubbleMaker/GGI_BubbleMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GGI_BubbleMakerApp.swift
+//  GGI BubbleMaker
+//
+//  Created by Luke Drushell on 3/16/23.
+//
+
+import SwiftUI
+
+@main
+struct GGI_BubbleMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
